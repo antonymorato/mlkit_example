688ecbc207104be1cd726f64539c9d3c8526af2f
Anton <antonymorato2@gmail.com>
2020-12-20 03:05:40 +0200

    added ml kit funcs

---
+++ android/app/src/main/kotlin/com/example/flutter_ml_rgr_ai/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutter_ml_rgr_ai
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,147 @@
+import 'dart:io';
+
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_ml_vision/firebase_ml_vision.dart';
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+
+import 'img_picker.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    Firebase.initializeApp();
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'ML Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: HomeScreen(),
+    );
+  }
+}
+
+class HomeScreen extends StatefulWidget {
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  String string = "TextRecognition";
+  File _userImageFile;
+  List<ImageLabel> _imageLabels = [];
+  List<Barcode> _barCode = [];
+  var result = "";
+
+  void _pickedImage(File image) {
+    _userImageFile = image;
+  }
+
+  //image_label_recognition
+
+//recognise_Text
+  recogniseText() async {
+    FirebaseVisionImage myImage = FirebaseVisionImage.fromFile(_userImageFile);
+    TextRecognizer recognizeText = FirebaseVision.instance.textRecognizer();
+    VisionText readText = await recognizeText.processImage(myImage);
+    result = "";
+    for (TextBlock block in readText.blocks) {
+      for (TextLine line in block.lines) {
+        setState(() {
+          result = result + ' ' + line.text + '\n';
+        });
+      }
+    }
+  }
+
+//barCode_Scanner
+  barCodeScanner() async {
+    FirebaseVisionImage myImage = FirebaseVisionImage.fromFile(_userImageFile);
+    BarcodeDetector barcodeDetector = FirebaseVision.instance.barcodeDetector();
+    _barCode = await barcodeDetector.detectInImage(myImage);
+    result = "";
+    for (Barcode barcode in _barCode) {
+      setState(() {
+        result = barcode.displayValue;
+      });
+    }
+  }
+
+//drawer_Item
+  Widget drawerItem(String title, IconData iconData, String _string) {
+    return InkWell(
+      onTap: () {
+        setState(() {
+          string = _string;
+        });
+        Navigator.of(context).pop();
+      },
+      child: Column(
+        children: [
+          ListTile(
+            leading: Icon(iconData),
+            title: Text(
+              title,
+              style: TextStyle(
+                fontWeight: FontWeight.w700,
+              ),
+            ),
+          ),
+          Divider()
+        ],
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        drawer: Drawer(
+          child: Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(18.0),
+                child: Image.network(
+                    "https://1.bp.blogspot.com/-B3K1G5D9sPQ/WvDZJGvkqVI/AAAAAAAAFSc/zx6VYIc0IXQmB8oR4c0i7SKjSNL-2xiTQCLcBGAs/s1600/ml-kit-logo.png"),
+              ),
+              Divider(),
+              drawerItem("Text recognition", Icons.title, "TextRecognition"),
+              drawerItem("Barcode scanning", Icons.workspaces_outline,
+                  "BarcodeScanning"),
+            ],
+          ),
+        ),
+        appBar: AppBar(
+          elevation: 0,
+          title: Text(string),
+          backgroundColor: Colors.blue[300],
+          actions: [
+            IconButton(
+              onPressed: () {
+                if (string == "TextRecognition") recogniseText();
+                if (string == "BarcodeScanning") barCodeScanner();
+                // if (string == "ImageLabeling") processImageLabels();
+              },
+              icon: Icon(Icons.check),
+            )
+          ],
+        ),
+        body: SingleChildScrollView(
+          child: Center(
+            child: Column(
+              children: [
+                UserImagePicker(_pickedImage),
+                Padding(
+                    padding: const EdgeInsets.all(16.0), child: Text(result)),
+              ],
+            ),
+          ),
+        ));
+  }
+}
